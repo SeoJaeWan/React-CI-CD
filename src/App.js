0c21c20918d2c2810fec5ff6da5293e95a30e5d3
lomen-SeoJaeWan <sjw734@lomen.kr>
2024-07-17 10:29:08 +0900

    feat: temp main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <img src="https://picsum.photos/300/300" />
+      <div>이미지 태그다</div>
+
+      <img src="https://picsum.photos/2000/3000" />
+      <div>큰 이미지다</div>
+
+      {new Array(10).fill(30).map(() => (
+        <img src="https://picsum.photos/4000/3000" />
+      ))}
+    </div>
+  );
 }
 
 export default App;
