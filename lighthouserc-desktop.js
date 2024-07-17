494213c4feaa2a9ad12f8ffec8e91fbc17008dcc
lomen-SeoJaeWan <sjw734@lomen.kr>
2024-07-17 10:16:30 +0900

    feat: lighthouse ci

---
+++ lighthouserc-desktop.js
@@ -0,0 +1,27 @@
+const {
+  LHCI_MONITORING_PAGE_NAMES,
+  getLhciUrlFromPageName,
+} = require("./src/configs/lighthouse/constant.js");
+
+const urls = LHCI_MONITORING_PAGE_NAMES.map(
+  (name) => `http://localhost:3000${getLhciUrlFromPageName(name)}`
+);
+
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      url: urls,
+      numberOfRuns: 1,
+      settings: {
+        preset: "desktop", // 🍥
+      },
+    },
+
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports/desktop", // 🍥
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+    },
+  },
+};
