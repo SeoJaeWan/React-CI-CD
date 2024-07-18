397e6e9da8f48df86858545badfa9654db495bf9
lomen-SeoJaeWan <sjw734@lomen.kr>
2024-07-18 10:51:04 +0900

    feat: update code

--- lighthouserc-desktop.js
+++ lighthouserc-desktop.js
@@ -1,7 +1,7 @@
-const {
+import {
   LHCI_MONITORING_PAGE_NAMES,
   getLhciUrlFromPageName,
-} = require("./src/configs/lighthouse/constant.js");
+} from "./src/configs/lighthouse/constant.js";
 
 const urls = LHCI_MONITORING_PAGE_NAMES.map(
   (name) => `http://localhost:3000${getLhciUrlFromPageName(name)}`
